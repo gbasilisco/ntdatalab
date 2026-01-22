b3dd9702d1a130e4e54de2b3888b975f5a4b8cdb
Giorgio Basilisco <gbasilisco@users.noreply.github.com>
2026-01-22 18:51:46 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { MakePortalLinkComponent } from './make-portal-link/make-portal-link.component';
+import { ManagePlayerComponent } from './manage-player/manage-player.component';
+import { ManageRolesComponent } from './manage-roles/manage-roles.component';
 
 export const routes: Routes = [
     { path: 'player', component: PlayerDetailsComponent, canActivate: [authGuard] },
@@ -12,9 +14,17 @@ export const routes: Routes = [
     { path: 'about', component: AboutComponent },
     { path: 'contact', component: ContactComponent },
     { path: 'make-portal-link', component: MakePortalLinkComponent },
+    { path: 'manage-lists/:id/players', component: ManagePlayerComponent },
+    { path: 'manage-roles', component: ManageRolesComponent },
     {
         path: 'manage-targets',
         loadComponent: () => import('./manage-targets/manage-targets.component').then(m => m.ManageTargetsComponent),
         canActivate: [authGuard]
+    },
+    {
+        path: 'manage-lists',
+        loadComponent: () => import('./manage-list/manage-list.component').then(m => m.ManageListComponent),
+        canActivate: [authGuard]
     }
+
 ];
